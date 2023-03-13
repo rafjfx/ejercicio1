386d640d460d23cc40a65258d4a67518fd269753
Rafael Jimenez <rafaeljimenez@Rafael-Jimenez.local>
2023-03-13 15:57:39 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int resultado;
+        resultado = suma(10, 30, 20);
+        System.out.println(resultado);
+    }
+    public static int suma(int a, int b, int c) {
+        return a + b + c;
+    }
+}
